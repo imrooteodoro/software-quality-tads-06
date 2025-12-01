de54740d119c981cbc555f5c6de2d1a21d576de1
imrooteodoro <adelsonteodoro67@gmail.com>
2025-11-30 22:06:50 -0300

    refactor: Remove Main class and its associated logic

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-import MaquinaRefri.MaquinaRefri;
-
-
-public class Main {
-    public static void main(String[] args) {
-        MaquinaRefri maquina = new MaquinaRefri();
-        maquina.iniciar();
-    }
-}
